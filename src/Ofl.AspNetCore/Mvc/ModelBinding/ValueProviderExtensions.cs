b0812c248248c26afcada2d246bbf1a2def9ec77
casperOne <casperOne@caspershouse.com>
2017-04-19 08:02:19 -0400

    Initial migration.

---
+++ src/Ofl.AspNetCore/Mvc/ModelBinding/ValueProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Linq;
+
+namespace Ofl.AspNetCore.Mvc.ModelBinding
+{
+    public static class ValueProviderExtensions
+    {
+        public static string GetSingleOrDefaultValue(this IValueProvider valueProvider, string key)
+        {
+            // Validate parameters.
+            if (valueProvider == null) throw new ArgumentNullException(nameof(valueProvider));
+
+            // Return single or default.
+            return valueProvider.GetValue(key).Values.SingleOrDefault();
+        }
+    }
+}
