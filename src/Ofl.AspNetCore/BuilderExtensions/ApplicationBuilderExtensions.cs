af7896e267fbfb5b99232e7190e9962b36b5d937
casperOne <casperOne@caspershouse.com>
2017-04-27 16:31:33 -0400

    Added AddHeaders to application builder extensions.

--- src/Ofl.AspNetCore/BuilderExtensions/ApplicationBuilderExtensions.cs
+++ src/Ofl.AspNetCore/BuilderExtensions/ApplicationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Primitives;
 
 namespace Ofl.AspNetCore.BuilderExtensions
 {
@@ -29,6 +30,34 @@ namespace Ofl.AspNetCore.BuilderExtensions
                 foreach (string header in hdrs)
                     // Remove.
                     context.Response.Headers.Remove(header);
+
+                // Return the call to next.
+                return next();
+            });
+        }
+
+        public static IApplicationBuilder AddHeaders(this IApplicationBuilder applicationBuilder,
+            params KeyValuePair<string, StringValues>[] headers) => applicationBuilder.AddHeaders((IEnumerable<KeyValuePair<string, StringValues>>) headers);
+
+        public static IApplicationBuilder AddHeaders(this IApplicationBuilder applicationBuilder,
+            IEnumerable<KeyValuePair<string, StringValues>> headers)
+        {
+            // Validate parameters.
+            if (applicationBuilder == null) throw new ArgumentNullException(nameof(applicationBuilder));
+            if (headers == null) throw new ArgumentNullException(nameof(headers));
+
+            // Materialize.
+            IReadOnlyCollection<KeyValuePair<string, StringValues>> hdrs = new ReadOnlyCollection<KeyValuePair<string, StringValues>>(headers.ToList());
+
+            // If there are no headers, get out, do nothing.
+            if (hdrs.Count == 0) return applicationBuilder;
+
+            // Create middleware and return.
+            return applicationBuilder.Use((context, next) => {
+                // Cycle through each item.
+                foreach (KeyValuePair<string, StringValues> header in hdrs)
+                    // Add.
+                    context.Response.Headers.Add(header);
 
                 // Return the call to next.
                 return next();
